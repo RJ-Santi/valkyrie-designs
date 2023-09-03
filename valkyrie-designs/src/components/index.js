7581de971f03253584112693efbb1f17b54d9521
Ryan Santiago <cr7rs7@gmail.com>
2023-09-03 19:44:40 -0400

    Add all component files for basic site structure.

---
+++ valkyrie-designs/src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Footer } from './Footer'
+export { default as Hero } from './Hero'
+export { default as Services } from './Services'
+export { default as Clients } from './Clients'
+export { default as Testimonials } from './Testimonials'
+export { default as Contact } from './Contact'
+export { default as Header } from './Header'
